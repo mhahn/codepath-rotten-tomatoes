63cb983bc618b4930b292e8e973c2fc524b7478f
Michael Hahn <mhahn@eventbrite.com>
2014-06-10 17:26:54 -0700

    Fade image thumbnails in

--- RottenTomatoes/RottenTomatoes/MovieTableViewCell.m
+++ RottenTomatoes/RottenTomatoes/MovieTableViewCell.m
@@ -34,7 +34,18 @@
     _movie = movie;
     self.movieTitleLabel.text = movie.title;
     self.movieSynopsisLabel.text = movie.synopsis;
-    [self.movieThumbnailImage setImageWithURL:[NSURL URLWithString:movie.posters[@"detailed"]]];
+    
+    // copied from AFNetworking so we can configure success animation
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:movie.posters[@"detailed"]]];
+    [request addValue:@"image/*" forHTTPHeaderField:@"Accept"];
+    [self.movieThumbnailImage setImageWithURLRequest:request placeholderImage:nil success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
+        self.movieThumbnailImage.alpha = 0.0;
+        self.movieThumbnailImage.image = image;
+        [UIView animateWithDuration:0.5 animations:^{
+            self.movieThumbnailImage.alpha = 1.0;
+        }];
+    } failure:nil];
+    
 }
 
 
